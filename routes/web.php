04a653d8099f680cdd2c18706b549790c23bac8c
brumenalkid-design <brumenalkid@gmail.com>
2026-01-12 16:45:17 -0300

    Atualizar o web.php

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return "Servidor Online - Chaves Geradas";
+    return "<h1>SISTEMA ONLINE</h1><p>Se voce esta vendo isso, as chaves foram aceitas. Agora remova este bloco do web.php para carregar o jogo.</p>";
 });
 
 include_once(__DIR__ . '/groups/layouts/app.php');
-
 
