807d1caa09da7c7600086d3050d125bf338f11c4
brumenalkid-design <brumenalkid@gmail.com>
2026-01-13 11:16:17 -0300

    Atualizar o app.php

--- config/app.php
+++ config/app.php
@@ -18,7 +18,6 @@ return [
     'providers' => [
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
-        Illuminate\Bus\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
         Illuminate\Cache\CacheServiceProvider::class,
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
