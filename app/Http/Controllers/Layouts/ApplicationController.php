b46c4593bd8d99ff178aae36132a0214d1a1e7b4
gamerdocker93-cpu <gamerdocker93@gmail.com>
2026-02-01 14:13:49 -0300

    Atualizar o ApplicationController.php

--- app/Http/Controllers/Layouts/ApplicationController.php
+++ app/Http/Controllers/Layouts/ApplicationController.php
@@ -7,65 +7,21 @@ use Illuminate\Http\Request;
 
 class ApplicationController extends Controller
 {
-
-    public function __invoke()
+    /**
+     * SPA entrypoint.
+     * Sempre retorna a view principal do Vue.
+     */
+    public function __invoke(Request $request, $view = null)
     {
         return view('layouts.app');
     }
 
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+    // Os métodos abaixo podem ficar, mas não são usados com rota invokable.
+    public function index() {}
+    public function create() {}
+    public function store(Request $request) {}
+    public function show(string $id) {}
+    public function edit(string $id) {}
+    public function update(Request $request, string $id) {}
+    public function destroy(string $id) {}
 }
