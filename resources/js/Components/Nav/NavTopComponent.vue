d7b75bb46da410a7feab0fb5b5ad08e1a53bf74b
gamerdocker93-cpu <gamerdocker93@gmail.com>
2026-02-01 16:20:50 -0300

    Add files via upload

--- resources/js/Components/Nav/NavTopComponent.vue
+++ resources/js/Components/Nav/NavTopComponent.vue
@@ -1,4 +1,3 @@
-
 <template>
     <nav class="fixed top-0 z-50 w-full navtop-color custom-box-shadow">
         <div class="px-3 lg:px-5 lg:pl-3 nav-menu">
@@ -889,5 +888,3 @@ export default {
 <style scoped>
 
 </style>
-
-
