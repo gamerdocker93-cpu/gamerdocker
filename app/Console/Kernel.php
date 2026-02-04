1596faee7719b0c392c7f21cb4b09d0e8f8c59c3
gamerdocker93-cpu <gamerdocker93@gmail.com>
2026-02-04 13:05:19 -0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         \App\Console\Commands\TempAdminCreate::class,
         \App\Console\Commands\FixAdminRole::class,
+        \App\Console\Commands\GamesKeysInit::class,
     ];
 
     /**
