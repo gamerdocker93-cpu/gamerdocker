4ff26b2cf7fdbe95ee9b7393c9620eff75da9168
gamerdocker93-cpu <gamerdocker93@gmail.com>
2026-02-05 17:49:41 -0300

    Add files via upload

--- resources/js/Components/Nav/NavTopComponent.vue
+++ resources/js/Components/Nav/NavTopComponent.vue
@@ -240,7 +240,7 @@
                         </div>
 
                         <div class="social-group mt-3">
-                            <a :href="redirectSocialTo()" class="text-green-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800">
+                            <a href="#" @click.prevent="goGoogle" class="text-green-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800">
                                 <i class="fa-brands fa-google"></i>
                             </a>
                         </div>
@@ -398,7 +398,7 @@
                         </div>
 
                         <div class="social-group mt-3">
-                            <a :href="redirectSocialTo()" class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800">
+                            <a href="#" @click.prevent="goGoogle" class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800">
                                 <i class="fa-brands fa-google"></i>
                             </a>
                         </div>
@@ -643,7 +643,11 @@ export default {
         });
     },
     methods: {
-        toggleSearch: function() {
+        goGoogle: function() {
+            // força navegação fora do Vue Router (hash mode)
+            window.location.assign('/auth/redirect/google');
+        },
+toggleSearch: function() {
             this.searchGameDataStore.setSearchGameToogle();
         },
         redirectSocialTo: function() {
