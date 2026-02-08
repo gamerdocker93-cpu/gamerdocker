4ed3f115510b157f7f095a8703681269cff55f2d
gamerdocker93-cpu <gamerdocker93@gmail.com>
2026-02-08 16:06:15 -0300

    Atualizar o deposit.php

--- routes/groups/api/wallet/deposit.php
+++ routes/groups/api/wallet/deposit.php
@@ -1,12 +1,26 @@
 <?php
 
 use App\Http\Controllers\Api\Wallet\DepositController;
+use App\Http\Controllers\Api\Gateways\StripeController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('deposit')
     ->group(function ()
     {
+        /*
+        |--------------------------------------------------------------------------
+        | Depósitos padrão (Pix / gateways atuais)
+        |--------------------------------------------------------------------------
+        */
         Route::get('/', [DepositController::class, 'index']);
         Route::post('/payment', [DepositController::class, 'submitPayment']);
-    });
 
+        /*
+        |--------------------------------------------------------------------------
+        | Stripe - Criar Checkout
+        |--------------------------------------------------------------------------
+        | Front chama:
+        | POST /api/wallet/deposit/stripe/checkout
+        */
+        Route::post('/stripe/checkout', [StripeController::class, 'createCheckout']);
+    });
