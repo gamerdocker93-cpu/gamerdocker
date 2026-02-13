6bb34812293ba5822eedf2d941786a2024372b63
gamerdocker93-cpu <gamerdocker93@gmail.com>
2026-02-13 19:09:58 -0300

    Create CreateGameProvider.php

---
+++ app/Filament/Admin/Resources/GameProviderResource/Pages/CreateGameProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Admin\Resources\GameProviderResource\Pages;
+
+use App\Filament\Admin\Resources\GameProviderResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateGameProvider extends CreateRecord
+{
+    protected static string $resource = GameProviderResource::class;
+}
+
