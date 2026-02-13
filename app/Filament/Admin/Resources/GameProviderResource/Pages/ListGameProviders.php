3911207ae65c34e1fb01b68159612a3627d95315
gamerdocker93-cpu <gamerdocker93@gmail.com>
2026-02-13 19:04:52 -0300

    Create ListGameProviders.php

---
+++ app/Filament/Admin/Resources/GameProviderResource/Pages/ListGameProviders.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Admin\Resources\GameProviderResource\Pages;
+
+use App\Filament\Admin\Resources\GameProviderResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListGameProviders extends ListRecords
+{
+    protected static string $resource = GameProviderResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
+
