e5ec1f2813780862b985249758806d2266df548d
gamerdocker93-cpu <gamerdocker93@gmail.com>
2026-02-13 18:11:52 -0300

    Atualizar o EditGameProvider.php

--- app/Filament/Resources/GameProviderResource/Pages/EditGameProvider.php
+++ app/Filament/Resources/GameProviderResource/Pages/EditGameProvider.php
@@ -3,17 +3,9 @@
 namespace App\Filament\Resources\GameProviderResource\Pages;
 
 use App\Filament\Resources\GameProviderResource;
-use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
 class EditGameProvider extends EditRecord
 {
     protected static string $resource = GameProviderResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\DeleteAction::make(),
-        ];
-    }
 }
