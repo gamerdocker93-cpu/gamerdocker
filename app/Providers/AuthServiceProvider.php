cd56ec8a4da91f5b0f74a93de47f2633f3350c0b
gamerdocker93-cpu <gamerdocker93@gmail.com>
2026-02-14 09:07:54 -0300

    Atualizar o AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use App\Models\GameProvider;
+use App\Policies\GameProviderPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -13,7 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        \App\Models\GameProvider::class => \App\Policies\GameProviderPolicy::class,
+        GameProvider::class => GameProviderPolicy::class,
     ];
 
     /**
